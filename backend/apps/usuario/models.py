b85c29b7557dadbaaeba8d4ff1e7628e72db53cc
Carlos Andres Ortiz Contreras <carlosaoc2611@gmail.com>
2026-02-12 22:11:47 -0500

    Permitido el acceso a Admin

--- backend/apps/usuario/models.py
+++ backend/apps/usuario/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import  AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-from datetime import datetime
+from django.utils import timezone
 
-class manageUsuario(BaseUserManager):
+class ManageUsuario(BaseUserManager):
     def create_user(self, identificacion, nombre, primerApellido, segundoApellido, correo, telefono, password, direccion , is_staff = False, is_superuser = False, is_active= True, **krows):
         usuario = self.model(
             identificacion = identificacion,
@@ -35,7 +35,7 @@ class manageUsuario(BaseUserManager):
             is_staff = is_staff,
             is_superuser = is_superuser,
             is_active = is_active,
-            date_joined = datetime.now(),
+            date_joined = timezone.now(),
             **krows
         )
         usuario.set_password(password)
@@ -49,12 +49,17 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     nombre = models.CharField(max_length= 50)
     primerApellido = models.CharField( max_length=40)
     segundoApellido= models.CharField(max_length= 40)
-    correo = models.EmailField
+    correo = models.EmailField()
     telefono = models.CharField(max_length= 10)
     direccion = models.TextField()
     is_active= models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    date_joined = datetime.now();
+    date_joined = models.DateTimeField(default= timezone.now)
+
+
     USERNAME_FIELD = 'identificacion'
+    REQUIRED_FIELDS = ['nombre', 'primerApellido', 'segundoApellido', 'correo', 'telefono', 'direccion']
+
+    objects = ManageUsuario()
 
