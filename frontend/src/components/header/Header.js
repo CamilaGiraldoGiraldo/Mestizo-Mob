35ab37af3468d9409d00c8f1318038f5e3aa3303
CamilaGiraldoGiraldo <133153684+CamilaGiraldoGiraldo@users.noreply.github.com>
2026-04-08 12:16:22 -0500

    nu

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -7,7 +7,7 @@ import { useAuth } from "../../context/AuthContext";
 import CartDrawer from "../cart/CartDrawer";
 import "./Header.css";
 
-const BASE = "https://mestizo-mob-3.onrender.com/usuario";
+const BASE = "http://127.0.0.1:8000/usuario";
 
 // ── Modal Login / Recuperación ───────────────────────────────
 function LoginModal({ onClose, onLoginSuccess }) {
